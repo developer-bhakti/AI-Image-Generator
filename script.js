0010c3dc01d6eff23a37407171094e12403c8cb3
purnale <bhaktipurnale89@gmail.com>
2024-07-12 17:33:02 +0530

    something

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-let generateImageForm = document.getElementById('generate-image-form');
+let generateImageForm = document.getElementById("generate-image-form");
+let formInput = document.getElementById("input-value");
+let imageContainerText = document.getElementById("imageContainerText");
+let imageGenerated = document.getElementById("generated-image");
+let imageContainer = document.getElementById("images-visible");
 
-let formInput = document.getElementById('input-value');
-
-let imageContainerText = document.getElementById('imageContainerText');
-
-let imageGenerated = document.getElementById('generated-image');
-
-let imageContainer = document.getElementById('images-visible');
-async function fatchImages(category) {
-    try {
-        let respose = await fatchImages('use a API');
-        if(!respose.ok) {
-            throw new Error('Unable to fatch the data');
-        }
-        imageContainerText.innerText = "Below is your generated Image:";
-        imageContainer.style.display = "block";
-        imageGenerated.src = respose.url;
-        console.log(respose.url);
+async function fetchImages(category) {
+  try {
+    let response = await fetch(
+      `  https://api.unsplash.com/photos/random?query=${category}&client_id=5UIoUinfB9mJzM6tuYq97Y6XSOaRbNF5R5jmzv2oYdY`
+    );
+    if (!response.ok) {
+      throw new Error("Unable to fetch the data");
     }
-catch (error) {
+    let data = await response.json();
+    imageContainerText.innerText = "Below is your generated Image:";
+    imageContainer.style.display = "block";
+    imageGenerated.src = data.urls.regular;
+    console.log(data.urls.regular);
+  } catch (error) {
     console.log(error);
- }
+  }
 }
-generateImageForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    let enteredText = formInput.value;
-    if(enteredText!==""){
-        fatchImages(enteredText);
-    }
-    else {
-        imageContainerText.innerText ="input field can not be empty!";
-    }
-})
+
+generateImageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let enteredText = formInput.value;
+  if (enteredText !== "") {
+    fetchImages(enteredText);
+  } else {
+    imageContainerText.innerText = "Input field cannot be empty!";
+  }
+});
